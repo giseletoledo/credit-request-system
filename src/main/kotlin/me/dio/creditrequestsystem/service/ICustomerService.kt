a9ce10e4459cd58cdcb014bd074838ea18648797
gisele <gisesonia@gmail.com>
2024-02-10 15:15:02 -0300

    imports extras

--- src/main/kotlin/me/dio/creditrequestsystem/service/ICustomerService.kt
+++ src/main/kotlin/me/dio/creditrequestsystem/service/ICustomerService.kt
@@ -1,8 +1,6 @@
 package me.dio.creditrequestsystem.service
 
-import me.dio.creditrequestsystem.entity.Credit
 import me.dio.creditrequestsystem.entity.Customer
-import org.hibernate.validator.constraints.UUID
 
 interface ICustomerService {
     fun save(customer: Customer): Customer
