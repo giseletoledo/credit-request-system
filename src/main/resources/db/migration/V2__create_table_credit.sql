f5d8f20fd2685ff8d4ce348123101bf9a4c6de4b
gisele <gisesonia@gmail.com>
2024-02-15 11:43:52 -0300

    alterando nome de campo

--- src/main/resources/db/migration/V2__create_table_credit.sql
+++ src/main/resources/db/migration/V2__create_table_credit.sql
@@ -3,7 +3,7 @@ CREATE TABLE credit (
    credit_code UUID NOT NULL,
    credit_value DECIMAL NOT NULL,
    day_first_installment date NOT NULL,
-   number_of_installment INT NOT NULL,
+   number_of_installments INT NOT NULL,
    status SMALLINT,
    customer_id BIGINT,
    CONSTRAINT pk_credit PRIMARY KEY (id)
